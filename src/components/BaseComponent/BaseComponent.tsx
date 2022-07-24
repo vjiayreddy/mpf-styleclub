a8de06e3fce761509011d73ad0a9781a5f0a78a9
DevCode <vijay1989reddy@gmail.com>
2022-07-24 10:07:03 +0530

    fixed issues

--- src/components/BaseComponent/BaseComponent.tsx
+++ src/components/BaseComponent/BaseComponent.tsx
@@ -1,4 +1,5 @@
 import { Theme } from "@mui/material";
+import Script from "next/script";
 import React from "react";
 
 interface BaseComponentProps {
@@ -9,37 +10,29 @@ const BaseComponent: React.FC<BaseComponentProps> = ({ theme }) => {
   return (
     <>
       <meta name="theme-color" content={theme.palette.primary.main} />
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600&display=swap"
+        rel="stylesheet"
+      ></link>
       <link rel="manifest" href="/manifest.json" />
       <link rel="apple-touch-icon" href="/icon.png" />
       <link rel="preload" href="/fonts/Inter-SemiBold.ttf" as="font" crossOrigin="anonymous" />
       <link rel="preload" href="/fonts/Inter-Regular.ttf" as="font" crossOrigin="anonymous" />
       <link
-        rel="preload"
-        as="style"
+        rel="stylesheet"
         type="text/css"
-        crossOrigin="true"
         href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
       />
       <link
-        rel="preload"
+        rel="stylesheet"
         type="text/css"
-        as="style"
-        crossOrigin="true"
         href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
       />
-      <link
-        rel="preload"
-        as="font"
-        crossOrigin="true"
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-      />
-      <link
-        rel="preload"
-        as="font"
-        crossOrigin="true"
-        href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600&display=swap"
-      ></link>
 
     </>
   );
