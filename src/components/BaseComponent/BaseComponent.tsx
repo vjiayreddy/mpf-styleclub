396e8722b28c053fb178c3c74e3e9e98737ac41d
DevCode <vijay1989reddy@gmail.com>
2022-07-24 09:40:23 +0530

    changed assects loading statagy

--- src/components/BaseComponent/BaseComponent.tsx
+++ src/components/BaseComponent/BaseComponent.tsx
@@ -1,5 +1,4 @@
 import { Theme } from "@mui/material";
-import Script from "next/script";
 import React from "react";
 
 interface BaseComponentProps {
@@ -16,32 +15,32 @@ const BaseComponent: React.FC<BaseComponentProps> = ({ theme }) => {
       <link rel="preload" href="/fonts/Inter-SemiBold.ttf" as="font" crossOrigin="anonymous" />
       <link rel="preload" href="/fonts/Inter-Regular.ttf" as="font" crossOrigin="anonymous" />
       <link
-        rel="preconnect"
-        href="https://fonts.gstatic.com"
-        crossOrigin="true"
-      />
-      <link
-        rel="stylesheet"
+        rel="preload"
+        as="style"
         type="text/css"
+        crossOrigin="true"
         href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
       />
       <link
-        rel="stylesheet"
+        rel="preload"
         type="text/css"
+        as="style"
+        crossOrigin="true"
         href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
       />
       <link
-        rel="stylesheet"
+        rel="preload"
+        as="font"
+        crossOrigin="true"
         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
       />
       <link
+        rel="preload"
+        as="font"
+        crossOrigin="true"
         href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600&display=swap"
-        rel="stylesheet"
       ></link>
-      <Script
-        src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-        strategy="lazyOnload"
-      />
+
     </>
   );
 };
